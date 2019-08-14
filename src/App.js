e26a3e5a98d9a9a90f163bd7f2d04bb2d8ecf484
Manuel <manuelalferezruiz@gmail.com>
2019-08-14 17:32:30 +0200

    feat: firebase and google analytics

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Grammar from "./components/Grammar/Grammar";
 import Vocabulary from "./components/Vocabulary/Vocabulary";
-/* import GA from "./GoogleAnalytics"; */
+import GA from "./GoogleAnalytics";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-{/*       {GA.init() && <GA.RouteTracker />} */}
+      {GA.init() && <GA.RouteTracker />}
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/grammar" component={Grammar} />
             <Route exact path="/vocabulary" component={Vocabulary} />
-           {/*  <Route exact path="/blog/post/:id" component={Post} /> */}
             <Route component={PageNotFound} />
           </Switch>
     </BrowserRouter>
