4530027398f19b3ac637e71dc3009a1bca4ea475
Manuel <manuelalferezruiz@gmail.com>
2019-08-14 17:06:24 +0200

    docs: new vocabulary and grammar

--- src/data/grammar.js
+++ src/data/grammar.js
@@ -13,7 +13,297 @@ const data = [
     question: "We  wish it  ____  the time of year when we could go on that hiking trip.",
     options: ["will be", "were", "is"],
     answer: "1"
-  }
+  },
+  {
+    question: "You  ____  to finish your assignment by tomorrow.",
+    options: ["should", "have", "must"],
+    answer: "1"
+  },
+  {
+    question: "William  ____  in South Africa now.",
+    options: ["is living", "was living", "will live"],
+    answer: "0"
+  },
+  {
+    question: "She is single. She never  ____",
+    options: ["marries", "marrying", "married"],
+    answer: "2"
+  },
+  {
+    question: "____  you like to join me this evening?",
+    options: ["Would", "Will", "Can"],
+    answer: "0"
+  },
+  {
+    question: "In those days, my grandfather  ____  never eat supper after seven o’clock.",
+    options: ["will", "used to", "would"],
+    answer: "2"
+  },
+  {
+    question: "There were  ____  than twenty people in the audience last week",
+    options: ["fewer", "lesser", "few"],
+    answer: "0"
+  },
+  {
+    question: "Excuse me, I’m lost.  ____  you help me?",
+    options: ["Should", "Shall", "Could"],
+    answer: "2"
+  },
+  {
+    question: "We could cook lunch.  ____  , we could buy some take away food.",
+    options: ["Therefore", "On the other hand", "Although"],
+    answer: "2"
+  },
+  {
+    question: "The kids  ____  playing hide and seek for two hours before it started raining.",
+    options: ["had been", "have been", "has been"],
+    answer: "0"
+  },
+  {
+    question: "She  ____  in Rio de Janeiro.",
+    options: ["living", "live", "lives"],
+    answer: "2"
+  },
+  {
+    question: "I read every day  ____  I’m not serious.",
+    options: ["but", "because", "and"],
+    answer: "0"
+  },
+  {
+    question: "A: I enjoy working on challenging assignments. B: So  ____  I!",
+    options: ["do", "am", "can"],
+    answer: "0"
+  },
+  {
+    question: "That man’s so unreliable – you can’t trust him.  ____",
+    options: ["I wouldn’t work frankly with him.", "Frankly, I wouldn’t work with him.", "I wouldn’t frankly work with him."],
+    answer: "1"
+  },
+  {
+    question: "How much  ____  the bus to Seville cost?",
+    options: ["is", "does", "do"],
+    answer: "1"
+  },
+  {
+    question: "The documentary starts  ____  half past four.",
+    options: ["on", "at", "in"],
+    answer: "1"
+  },
+  {
+    question: "If you were a better handyman, we  ____  need to the plumber all the time.",
+    options: ["hadn’t", "wouldn’t", "won’t"],
+    answer: "1"
+  },
+  {
+    question: "You  ____  have caught the early bus, as the football match was delayed.",
+    options: ["needn’t", "mustn’t", "didn’t"],
+    answer: "1"
+  },
+  {
+    question: "She  ____  finished cleaning when we arrived.",
+    options: ["has", "have", "had"],
+    answer: "2"
+  },
+  {
+    question: "Mark  ____  to work when he saw Kevin.",
+    options: ["going", "went", "was going"],
+    answer: "2"
+  },
+  {
+    question: "They found the hole  _____  the treasure had been buried.",
+    options: ["–", "where", "that"],
+    answer: "1"
+  },
+  {
+    question: "Don’t wait for us. Our friends told us  ____",
+    options: ["don’t wait", "didn’t wait", "not to wait"],
+    answer: "2"
+  },
+  {
+    question: "The Friday morning lectures are usually quite  ____",
+    options: ["boring", "bored", "not to wait"],
+    answer: "0"
+  },
+  {
+    question: "Go to the end of the street and turn  ____  right",
+    options: ["on", "to", "-"],
+    answer: "2"
+  },
+  {
+    question: "I  ____  use to swim much, but now I have my onw pool, I swim every day.",
+    options: ["don´t", "didn´t", "did"],
+    answer: "1"
+  },
+  {
+    question: "The children  ____  playing football for two hours before it started raining.",
+    options: ["has been", "have been", "had been"],
+    answer: "2"
+  },
+  {
+    question: "Excuse me, I´m lost.  ____  you help me?",
+    options: ["Could", "Shall", "Should"],
+    answer: "0"
+  },
+  {
+    question: "There were  ____  than fifty people in the audience last night.",
+    options: ["few", "lesser", "fewer"],
+    answer: "2"
+  },
+  {
+    question: "We  ____  have caught the early train, as the football match was delayed.",
+    options: ["didn´t", "needn´t", "mustn´t"],
+    answer: "1"
+  },
+  {
+    question: "I just heard they are  ____  get married in June.",
+    options: ["go to", "going", "going to"],
+    answer: "2"
+  },
+  {
+    question: "She reads every day  ____  she isn´t serious.",
+    options: ["and", "but", "because"],
+    answer: "1"
+  },
+  {
+    question: "He  ____  in Cairo.",
+    options: ["living", "lives", "live"],
+    answer: "1"
+  },
+  {
+    question: "He  ____  finished cooking when we arrived.",
+    options: ["has", "have", "had"],
+    answer: "2"
+  },
+  {
+    question: "How much  ____  the train to Santiago cost?",
+    options: ["does", "do", "is"],
+    answer: "0"
+  },
+  {
+    question: "I don´t think it´s right  ____  children to play with toy guns.",
+    options: ["for", "that", "to"],
+    answer: "0"
+  },
+  {
+    question: "I had a shower,  ____  I went to bed.",
+    options: ["then", "during", "until"],
+    answer: "0"
+  },
+  {
+    question: "If you were a better cook, you  ____  need to eat out all the time.",
+    options: ["hadn´t", "wont´t", "wouldn´t"],
+    answer: "2"
+  },
+  {
+    question: "____  you like to go out this evening?",
+    options: ["Would", "Will", "Can"],
+    answer: "0"
+  },
+  {
+    question: "He is single. He never  ____.",
+    options: ["marrying", "married", "marries"],
+    answer: "1"
+  },
+  {
+    question: "The film starts  ____  half past four.",
+    options: ["at", "in", "on"],
+    answer: "0"
+  },
+  {
+    question: "I  ____  to work when I saw Steve.",
+    options: ["went", "going", "was going"],
+    answer: "2"
+  },
+  {
+    question: "We could cook dinner.  ____,  we could buy some take away food.",
+    options: ["Although", "On the other hand", "Therefore"],
+    answer: "1"
+  },
+  {
+    question: "I wish it  ____  the time of year when I could go on holiday.",
+    options: ["were", "will be", "is"],
+    answer: "0"
+  },
+  {
+    question: "You  ____  to do your homework by tomorrow.",
+    options: ["have", "must", "should"],
+    answer: "0"
+  },
+  {
+    question: "In those days, my father  ____  never eat dinner after eight o´clock.",
+    options: ["used to", "would", "will"],
+    answer: "1"
+  }/* ,
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  },
+  {
+    question: "",
+    options: ["", "", ""],
+    answer: ""
+  } */
 ];
 
 export default data;
--- src/data/vocabulary.js
+++ src/data/vocabulary.js
@@ -33,7 +33,227 @@ const data = [
         question: 'to discover or perceive after consideration is to ',
         options: ['find','boost','increase','gather','assemble'],
         answer: '0'
-    }
+    },
+    {
+        question: 'to bring together into one group, collection, or place is to ',
+        options: ['find','boost','increase','gather','assemble'],
+        answer: '3'
+    },
+    {
+        question: 'to put or fit together; put together the parts of is to ',
+        options:  ['find','boost','increase','gather','assemble'],
+        answer: '4'
+    },
+    {
+        question: 'to make greater, as in number, size, strength, or quality; augment; add to is to',
+        options:  ['find','boost','increase','gather','assemble'],
+        answer: '2'
+    },
+    {
+        question: 'to increase; raise or to',
+        options: ['find','boost','increase','gather','assemble'],
+        answer: '1'
+    },
+    {
+        question: 'The paper  ____  was followed by a reform of the metallic currency.',
+        options: ['spit','easy','currency','shortage','wild'],
+        answer: '2'
+    },
+    {
+        question: 'A  ____  of teachers has put thousands of students in overcrowded classrooms.',
+        options: ['spit','easy','currency','shortage','wild'],
+        answer: '3'
+    },
+    {
+        question: '____ cats roamed around the gas station',
+        options: ['spit','easy','currency','shortage','wild'],
+        answer: '4'
+    },
+    {
+        question: 'This isn’t  ____  for you, is it?',
+        options:['spit','easy','currency','shortage','wild'],
+        answer: '1'
+    },
+    {
+        question: 'The young man’s  ____  flew through the air and hit the pavement.',
+        options: ['spit','easy','currency','shortage','wild'],
+        answer: '0'
+    },
+    {
+        question: 'calf',
+        options: ['wonderful','urge','silly','baby cow','courageous'],
+        answer: '3'
+    },
+    {
+        question: 'amazing',
+        options: ['wonderful','urge','silly','baby cow','courageous'],
+        answer: '0'
+    },
+    {
+        question: 'brave',
+        options: ['wonderful','urge','silly','baby cow','courageous'],
+        answer: '4'
+    },
+    {
+        question: 'funny',
+        options: ['wonderful','urge','silly','baby cow','courageous'],
+        answer: '2'
+    },
+    {
+        question: 'hurry',
+        options: ['wonderful','urge','silly','baby cow','courageous'],
+        answer: '1'
+    },
+    {
+        question: 'black',
+        options: ['and soul','dog','English','review','hole'],
+        answer: '4'
+    },
+    {
+        question: 'book',
+        options: ['and soul','dog','English','review','hole'],
+        answer: '3'
+    },
+    {
+        question: 'heart',
+        options: ['and soul','dog','English','review','hole'],
+        answer: '0'
+    },
+    {
+        question: 'old',
+        options: ['and soul','dog','English','review','hole'],
+        answer: '2'
+    },
+    {
+        question: 'police',
+        options:['and soul','dog','English','review','hole'],
+        answer: '1'
+    },
+    {
+        question: 'big',
+        options: ['go','begin','plan','listen','large'],
+        answer: '4'
+    },
+    {
+        question: 'study',
+        options: ['go','read','plan','listen','large'],
+        answer: '1'
+    },
+    {
+        question: 'receive',
+        options: ['go','begin','plan','get','large'],
+        answer: '3'
+    },
+    {
+        question: 'talk',
+        options: ['go','begin','plan','get','large','speak'],
+        answer: '5'
+    },
+    {
+        question: 'To cover with paper or material is to',
+        options: ['attend','assemble','approach','arrive','admit','announce','arrange','adjust','accept','wrap'],
+        answer: '9'
+    },
+    {
+        question: 'To state or make public is to',
+        options: ['attend','assemble','approach','arrive','admit','announce','arrange','adjust','accept','wrap'],
+        answer: '5'
+    },
+    {
+        question: 'To get somewhere is to',
+        options: ['attend','assemble','approach','arrive','admit','announce','arrange','adjust','accept','wrap'],
+        answer: '3'
+    },
+    {
+        question: 'To put something together is to',
+        options: ['attend','assemble','approach','arrive','admit','announce','arrange','adjust','accept','wrap'],
+        answer: '1'
+    },
+    {
+        question: 'More and more people go  ____  for their holidays these days.',
+        options: ['awkward','ancient','ahead','adequate','articulate','alert','accurate','abroad','adult','agressive'],
+        answer: '7'
+    },
+    {
+        question: 'Drivers need to remain  ____  when in charge of a car.',
+        options: ['awkward','ancient','ahead','adequate','articulate','alert','accurate','abroad','adult','agressive'],
+        answer: '5'
+    },
+    {
+        question: 'The city has many  ____  and interesting buildings.',
+        options:['awkward','ancient','ahead','adequate','articulate','alert','accurate','abroad','adult','agressive'],
+        answer: '1'
+    },
+    {
+        question: 'She´s a highly  ____  person who explains things clearly.',
+        options: ['awkward','ancient','ahead','adequate','articulate','alert','accurate','abroad','adult','agressive'],
+        answer: '4'
+    },
+    {
+        question: 'He´s a very  ____  person - always getting into fights.',
+        options: ['awkward','ancient','ahead','adequate','articulate','alert','accurate','abroad','adult','agressive'],
+        answer: '9'
+    },
+    {
+        question: 'timetable',
+        options: ['line','joint','command','prize','mess','fright','schedule','dock','stage','mistake'],
+        answer: '6'
+    },
+    {
+        question: 'reward',
+        options: ['line','joint','command','prize','mess','fright','schedule','dock','stage','mistake'],
+        answer: '3'
+    },
+    {
+        question: 'queue',
+        options: ['line','joint','command','prize','mess','fright','schedule','dock','stage','mistake'],
+        answer: '0'
+    },
+    {
+        question: 'phase',
+        options: ['line','joint','command','prize','mess','fright','schedule','dock','stage','mistake'],
+        answer: '8'
+    },
+    {
+        question: 'error',
+        options: ['line','joint','command','prize','mess','fright','schedule','dock','stage','mistake'],
+        answer: '9'
+    },
+    {
+        question: 'big + ',
+        options: ['bomb','audition','scissors','rod','classes','strings','salesman','seep','minister','seat','house'],
+        answer: '10'
+    },
+    {
+        question: 'aerobics + ',
+        options: ['bomb','audition','scissors','rod','classes','strings','salesman','seep','minister','seat','house'],
+        answer: '4'
+    },
+    {
+        question: 'agriculture + ',
+        options: ['bomb','audition','scissors','rod','classes','strings','salesman','seep','minister','seat','house'],
+        answer: '2'
+    },
+    {
+        question: 'aisle + ',
+        options: ['bomb','audition','scissors','rod','classes','strings','salesman','seep','minister','seat','house'],
+        answer: '9'
+    },
+    {
+        question: 'apron + ',
+        options: ['bomb','audition','scissors','rod','classes','strings','salesman','seep','minister','seat','house'],
+        answer: '5'
+    },
+    {
+        question: 'atom + ',
+        options: ['bomb','audition','scissors','rod','classes','strings','salesman','seep','minister','seat','house'],
+        answer: '0'
+    }/* ,
+    {
+        question: '',
+        options: ['','','','',''],
+        answer: ''
+    } */
 ]
 
 export default data;
